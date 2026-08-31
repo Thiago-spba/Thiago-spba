22339a0bd42589a7bd47b899c329a57bc4134977
Thiago Santos <thiago.rpba@gmail.com>
2026-08-31 10:53:07 -0300

    fix: atualizar credenciais reais no src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,19 @@
-﻿import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  "apiKey": "AIzaSyBg2AEb82yO5Sk2TPuITfdPRscoDr-P2P8",
+  "authDomain": "controle-de-aulas-c2973.firebaseapp.com",
+  "projectId": "controle-de-aulas-c2973",
+  "storageBucket": "controle-de-aulas-c2973.firebasestorage.app",
+  "messagingSenderId": "662572820697",
+  "appId": "1:662572820697:web:0d15bd23d25786a6cb76c8"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
 export { signInWithPopup, signOut };
